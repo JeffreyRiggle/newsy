cd4288081f9922012836f46119abe96ab55c36d8
JeffreyRiggle <JeffreyRiggle@gmail.com>
2018-08-02 06:14:42 -0400

    Move httpclient out to its own module

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,9 @@ extern crate serde;
 extern crate serde_json;
 extern crate futures;
 
-use hyper::{Body, Request};
-use hyper::Client;
-use hyper_tls::HttpsConnector;
-use hyper::rt::{self, Future, Stream};
+use hyper::rt::{self, Future};
+pub use self::httpclient::HttpClient;
+mod httpclient;
 
 #[derive(Serialize, Deserialize)]
 struct Commit {
@@ -105,45 +104,5 @@ impl GitClient {
         .map_err(|err| {
             eprintln!("Error {}", err);
         })
-    }
-}
-
-pub struct HttpClient {
-
-}
-
-impl HttpClient {
-    fn make_request<T>(method: &str, url: &str) -> impl Future<Item = T, Error = hyper::Error>
-    where 
-        T: serde::de::DeserializeOwned, 
-    {
-        let client = HttpClient::create_client();
-        
-        let req = HttpClient::build_request(method, url);
-    
-        client.request(req)
-              .and_then(|res| {
-                  println!("Got response: {}", res.status());
-                  res.into_body().concat2()
-               })
-               .and_then(move |body| {
-                   let s = String::from_utf8(body.to_vec()).unwrap();
-                   let ds = serde_json::from_str(&s).unwrap();
-                   Ok(ds)
-               })
-    }
-
-    fn create_client() -> hyper::Client<hyper_tls::HttpsConnector<hyper::client::HttpConnector>, hyper::Body> {
-        let https = HttpsConnector::new(4).expect("TLS initialization failed");
-        Client::builder().build::<_, hyper::Body>(https)
-    }
-
-    fn build_request(method: &str, url: &str) -> hyper::Request<hyper::Body> {
-        Request::builder()
-            .method(method)
-            .uri(url)
-            .header("user-agent", "newsy")
-            .body(Body::empty())
-            .unwrap()
     }
 }
